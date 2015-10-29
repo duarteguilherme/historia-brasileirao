4c19b907d607247cb5124178a5548b72d87400be
Rodrigo Burgarelli <rodrigo.burgarelli1@gmail.com>
2015-10-29 16:18:02 -0200

    data fake e script

---
+++ data_fake.py
@@ -0,0 +1,39 @@
+import json
+from datetime import *
+
+def date_range(start, end, intv):
+    diff = (end  - start ) / intv
+    for i in range(intv):
+        yield (start + diff * i).strftime("%Y-%m-%d")
+    yield end.strftime("%Y-%m-%d")
+
+with open('visualizacao/data/dadosfull.json','r') as file:
+    dados = json.load(file)
+    datas = [datetime.strptime(d,'%Y-%m-%d').date() for d in dados['data']]
+    n_dados = {}
+    for d in datas:
+        if d.year not in n_dados:
+            n_dados[d.year] = []
+        n_dados[d.year].append(d)
+
+    dados_saida = []
+    for ano in n_dados:
+        tamanho = len(n_dados[ano])
+        start = date(ano,1,1)
+        end = date(ano,11,30)
+        novas_datas = date_range(start,end,tamanho-1)
+        novas_datas = [n for n in novas_datas]
+        dados_saida += novas_datas
+
+    dados['data_fake'] = dados_saida
+
+    with open('visualizacao/data/dadosfullVALEESTE.json','w') as outfile:
+        json.dump(dados,outfile)
+
+
+
+
+
+
+
+
