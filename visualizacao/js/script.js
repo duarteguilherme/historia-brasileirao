f38f7aa9f652ed697bbbda51b32ea7b8f5365249
yurialexsander <yuriatp@gmail.com>
2015-10-30 20:28:47 -0200

    Aumenta as dimensões do gráfico, adiciona novos detalhes de usabilidade e inclui menu com escudos dos times.

--- visualizacao/js/script.js
+++ visualizacao/js/script.js
@@ -10,13 +10,15 @@ function printa(string) {
 }
 
 //DIMENSOES DO GRAFICO
-var margin = {top: 20, right: 80, bottom: 30, left: 50},
-    width = 1024 - margin.left - margin.right,
+var margin = {top: 10, right: 55, bottom: 30, left: 50},
+    // width = 1040 - margin.left - margin.right,
+    w = window.innerWidth - 20,
+    width = window.innerWidth - margin.right - margin.left - 20,
     height = 500 - margin.top - margin.bottom;
 
 //CODIGO REFERENTE AO ZOOM
 var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 8])
+    .scaleExtent([1, 6])
     .on("zoom", zoomed);
 
 //CODIGO REFERENTE AO DRAG
@@ -41,7 +43,8 @@ var xAxis = d3.svg.axis()
     .scale(x)
     //.tickSize(-height, 0)
     //.tickPadding(6)
-    .orient("bottom");
+    .orient("bottom")
+    .ticks(d3.time.years, 2);
 
 //DE QUE MANEIRA AS INFORMACOES DO EIXO Y VAO APARECER 
 var yAxis = d3.svg.axis()
@@ -84,29 +87,36 @@ var linhaSelecionada; //ARMAZENA A LINHA ESCOLHIDA A PARTIR DO TIME ESCOLHIDO PE
 var nomesDosTimes; //ARMAZENA O NOME DOS TIMES; VARIAVEL DE APOIO PARA CHECAGEM E ITERACAO
 var times; //ARMAZENA CADA OBJETO-TIME INTEIRO COM SEUS RESPECTIVOS VALORES (DATAS E INDICES)
 var linhaTime; //ARMAZENA A LINHA ESPECIFICA DE CADA TIME
-var tooltip; //TOOLTIP COM INFORMACOES A SEREM APRESENTADAS AO USUARIO
 var container; //CONTAINER PARA ENVOLVER OS ELEMENTOS INTERNOS DO SVG
 var circulos; //ADICIONADOS AO GRAFICO QUANDO O USUARIO SELECIONA UMA LINHA ESPECIFICA
+//TOOLTIP COM INFORMACOES A SEREM APRESENTADAS AO USUARIO
+var tooltip = d3.select("body").append("div")
+      .attr("class", "infoTooltip")
+      .style("position", "absolute")
+      .style("padding", "4px 8px")
+      .style("background", function(d) {return "rgb(239,239,239)"})
+      .style("opacity", 0);
 
 //TRABALHANDO COM OS DADOS DO ARQUIVO .JSON
-d3.json ("data/novodadosfull.json", function(error, dados) {
+d3.json ("data/dadosfull.json", function(error, dados) {
   
   if (error) throw error;
 
   //ARMAZENANDO O TOTAL DE TIMES
   nomesDosTimes = d3.keys(dados).filter(function(index, element) { return index !== "data"; });
+  nomesDosTimes.splice(nomesDosTimes.indexOf("data_fake"), 1);
   
   //INSERINDO O NOME DOS CLUBES NO DROPDOWN MENU
   nomesDosTimes.forEach(function(d) {
 
-    $("ul.sub-menu").append("<li class=\"timeBrasileirao\"><a href=\"#\">" + d + "</a></li>");
+    $("ul.sub-menu").append("<li class=\"timeBrasileirao" + " " + d + "\"><a href=\"javascript:void(0)\">" + d + "</a></li>");
 
   });
 
   //PARSEANDO AS DATAS PARA O FORMATO DE LEITURA DO GRAFICO
-  dados["data"].forEach(function(data, index) {
+  dados["data_fake"].forEach(function(data, index) {
 
-    dados["data"][index] = parseData(data);
+    dados["data_fake"][index] = parseData(data);
 
   });
 
@@ -116,23 +126,24 @@ d3.json ("data/novodadosfull.json", function(error, dados) {
     return {
 
       nome : nomeTime,
-      valores : dados["data"].map (function(d, index) {
+      valores : dados["data_fake"].map (function(d, index) {
 
         return {
 
-          data: dados["data"][index],
+          data: dados["data_fake"][index],
+          data_real: dados["data"][index],
           indice: dados[nomeTime][index]
 
         };
       })
     }; 
   });
-  
+
   //PARA LEMBRAR: COMO ACESSAR OS ITENS NA VARIAVEL TIMES
   //console.log(times[0].valores[0].indice);
   
   //ESTABELECENDO O DOMINIO DE VALORES DO EIXO X A PARTIR DAS DATAS
-  x.domain(d3.extent(dados["data"], function(d) { return d; }));
+  x.domain(d3.extent(dados["data_fake"], function(d) { return d; }));
 
   //ESTABELECENDO O DOMINIO DE VALORES DO EIXO Y 
   //A PARTIR DOS INDICES
@@ -152,7 +163,7 @@ d3.json ("data/novodadosfull.json", function(error, dados) {
 
   //CRIACAO DO CONTAINER PARA INCLUIR OS ELEMENTOS SEGUINTES
   container = svg.append("g")
-               .attr("class", "container");
+                .attr("class", "container");
 
   //CONTINUANDO O DESENHO DO GRAFICO
   //ADICIONANDO O EIXO X
@@ -180,50 +191,35 @@ d3.json ("data/novodadosfull.json", function(error, dados) {
     .enter().append("g")
     .attr("class", "linhaTime");
 
-  //CRIANDO A VARIAVEL DE TOOLTIP COM AS INFORMACOES DAS LINHAS
-  var tooltip = d3.select("body").append("div")
-        .attr("class", "infoTooltip")
-        .style("position", "absolute")
-        .style("padding", "4px 8px")
-        .style("background", function(d) {return "rgb(239,239,239)"})
-        .style("opacity", 0);
-
   //DESENHANDO AS LINHAS
   linhaTime.append("path")
       .attr("class", "linha")
       .attr("d", function(d) {
-        
+
         return line(d.valores); 
 
       })
       .attr("opacity", 0.7)
       .style("stroke-width", function(d) { return ".5px"; })
-      //.style("position", "relative")
       .style("stroke", function(d) { return "rgb(127, 127, 127)"; })
 
   //CRIANDO O EVENTO PARA MOSTRAR A INFORMACAO DA LINHA
   //AO PASSAR O MOUSE PELA LINHA
-  .on("mouseover", function(d, i) {
+  .on("mouseover", function(element, i) {
 
     if(zoom.scale() > 4 && $(".circulo").length > 0) {
-      var distanciaX = d3.mouse(this)[0];
-      var circuloMaisProximo = achaCirculoMaisProximo(nomesDosTimes[i], distanciaX);
 
-      tooltip.transition()
-      .style("opacity", .9)
-      .text(nomesDosTimes[i] + " " + circuloMaisProximo.indice)
-      tooltip.html(d)
-      .style("left", (d3.event.pageX+5) + "px")
-      .style("top", (d3.event.pageY-30) + "px")
     }
 
     else {
+
       tooltip.transition()
       .style("opacity", .9)
       .text(nomesDosTimes[i])
-      tooltip.html(d)
+      tooltip
       .style("left", (d3.event.pageX+5) + "px")
-      .style("top", (d3.event.pageY-30) + "px")
+      .style("top", (d3.event.pageY-30) + "px");
+
     }
 
   })
@@ -233,51 +229,89 @@ d3.json ("data/novodadosfull.json", function(error, dados) {
   .on("mouseout", function(d, i) {
     tooltip.style("opacity", 0)
   })
-  
-  //ELEMENTO PARA AGRUPAR OS CIRCULOS DE REFERENCIA
-    circulos = container.append("g")
-    .attr("class", "circulos");
+
+//ELEMENTO PARA AGRUPAR OS CIRCULOS DE REFERENCIA
+  circulos = container.append("g")
+  .attr("class", "circulos");
+
+  //CAPTURA DE MOVIMENTO DENTRO DO SVG
+  var w = d3.select("svg")
+      .on("mousemove", mousemove);
 
 });
 
 //FUNCAO CHAMADA QUANDO O DOCUMENTO ESTA PRONTO
 $(document).ready(function(){
 
-  //CASO ALGUM ITEM O MENU SEJA SELECIONADO
-  $(".sub-menu li").click(function(event) {
+  //CASO ALGUM ITEM DO MENU SEJA SELECIONADO
+  $(".sub-menu").click(function(event) {
 
     //ARMAZENA O INDEX DO TIME ESCOLHIDO
-    timeEscolhido = $(this).index();
+    timeEscolhido = $(event.target).parent().index();
 
     //ARMAZENA A LINHA ESCOLHIDA
     linhaSelecionada = $(".linhaTime .linha")[timeEscolhido];
     //linhaSelecionada = d3.selectAll(".linhaTime .linha")[0][timeEscolhido]; //MESMO EFEITO REALIZADO COM D3
 
-    //DIMINUI A OPACIDADE DE TODAS AS LINHAS
-    //RETORNA A COR DE TODAS AS LINHAS PARA O CINZA
-    $(".linhaTime .linha").attr("opacity", function(d) {return 0.4});
-    $(".linhaTime .linha").css("stroke", function(d) { return "rgb(127, 127, 127)"});
-    $(".linhaTime .linha").css("stroke-width", function(d) { return ".5px"});
-    //d3.selectAll(".linhaTime .linha").attr("opacity", function(d) {return 0.1}); //MESMO EFEITO REALIZADO COM D3
+    mostraLinha(timeEscolhido, linhaSelecionada);
 
-    //AUMENTA A OPACIDADE DA LINHA SELECIONADA
-    //ALTERA A COR DA LINHA SELECIONADA
-    //DESLOCA ELA PARA CIMA DAS OUTRAS
-    //var ultimaLinha = $(".linhaTime .linha")[$(".linhaTime .linha").length - 1];
-    //$(linhaSelecionada).insertAfter(ultimaLinha);
-    $(linhaSelecionada).parent().addClass("linhaSelecionada");
-    $(linhaSelecionada).attr("opacity", 1);
-    $(linhaSelecionada).css("stroke", function(d) { return "rgb(255, 102, 0)" });
-    $(linhaSelecionada).css("stroke-width", function(d) { return ".5px" });
-    
-    //ADICIONA OS CIRCULOS DE REFERENCIA DA LINHA SELECIONADA    
-    criaCirculos(timeEscolhido);
+  });
+
+  $(".menuEscudos li").click(function(e) {
+    //e.preventDefault();
+    var escudoSelecionado = $(this);
+
+    //CHECA O TIME SELECIONADO PELO ID DO ESCUDO
+    times.forEach(function(time, index){
+      
+      if (time.nome == escudoSelecionado.attr("id")) {
+
+        timeEscolhido = index;
+        
+        //ARMAZENA A LINHA ESCOLHIDA
+        linhaSelecionada = $(".linhaTime .linha")[timeEscolhido];
+
+        //APAGA AS LINHAS SENDO EM EVIDENCIA E MOSTRA A SELECIONADA
+        //ANIMA A TELA
+        mostraLinha(timeEscolhido, linhaSelecionada);
+
+      }
+
+    });
+
   });
 
 });
 
+function mostraLinha (timeEscolhido, linhaSelecionada) {
+
+  //DIMINUI A OPACIDADE DE TODAS AS LINHAS
+  //RETORNA A COR DE TODAS AS LINHAS PARA O CINZA
+  $(".linhaTime .linha").attr("opacity", function(d) {return 0.4});
+  $(".linhaTime .linha").css("stroke", function(d) { return "rgb(127, 127, 127)"});
+  $(".linhaTime .linha").css("stroke-width", function(d) { return ".5px"});
+
+  //AUMENTA A OPACIDADE DA LINHA SELECIONADA
+  //ALTERA A COR DA LINHA SELECIONADA
+  //DESLOCA ELA PARA CIMA DAS OUTRAS
+  $(linhaSelecionada).attr("opacity", 1);
+  $(linhaSelecionada).css("stroke", "rgb(255, 102, 0)");
+  $(linhaSelecionada).css("stroke-width", function(d) { return ".5px" });
+
+  //ADICIONA OS CIRCULOS DE REFERENCIA DA LINHA SELECIONADA    
+  criaCirculos(timeEscolhido);
+
+  //ADICIONANDO A LEGENDA ACIMA DO GRAFICO
+  $("#nomeTimeSelecionado").text(times[timeEscolhido].nome /*+ " | Fundado em: | Vencedor de x Campeonatos Brasileiros"*/);
+
+  $('html, body').delay(60).animate({
+     scrollTop: $("#menuEscudos01").offset().top
+  }, 500, 'easeInOutCubic');
+
+}
+
 //FUNCOES REFERENTES AO ZOOM/DRAG
-function zoomed() {
+function zoomed () {
 
   var t = d3.event.translate,
       s = d3.event.scale;
@@ -333,24 +367,6 @@ function clickGrafico() {
 
 }
 
-//FUNCAO PARA ACHAR O CIRCULO DE REFERENCIA
-//MAIS PROXIMO DO MOUSE DO USUARIO
-function achaCirculoMaisProximo (nomeDoTime, distanciaX) {
-
-  var maximo = width;
-  var objeto;
-  d3.selectAll("circle." + nomeDoTime).each(function (d) {
-      var distancia = Math.abs(d3.select(this).attr("cx") - distanciaX);
-      if (distancia < maximo) {
-          maximo = distancia;
-          objeto = d;
-      }
-  })
-
-  return objeto;
-
-}
-
 //ADICIONA OS CIRCULOS DE REFERENCIA DA LINHA SELECIONADA    
 function criaCirculos (timeEscolhido) {
 
@@ -381,3 +397,63 @@ function criaCirculos (timeEscolhido) {
 function removeCirculos () {
   $(".circulo").remove();
 }
+
+function mousemove() {
+
+  if(zoom.scale() > 4 && $(".circulo").length > 0) {
+
+    var distanciaMouseX = d3.mouse(this)[0];
+    var circuloMaisProximo = achaCirculoMaisProximo(nomesDosTimes[timeEscolhido], distanciaMouseX);
+
+    if (circuloMaisProximo.indice != null) {
+
+      /* PAREI AQUI
+      var nomeDaLinhaParaMostrar = element.nome;
+      var indexDaLinhaParaMostrar;
+      
+      nomesDosTimes.forEach(function (element, index) {
+        if (nomeDaLinhaParaMostrar == element) {
+          indexDaLinhaParaMostrar = index;
+        }
+      });
+
+      console.log(linhaTime[0][indexDaLinhaParaMostrar].firstChild);
+
+      //d.style("stroke", function(d) { return "rgb(127, 1, 234)"; });
+      */
+
+      tooltip.transition()
+      .style("opacity", .9)
+      .text(nomesDosTimes[timeEscolhido] + " : " + circuloMaisProximo.indice)
+      tooltip
+      .style("left", (d3.event.pageX+5) + "px")
+      .style("top", (d3.event.pageY-30) + "px")
+
+    }
+
+  }
+
+}
+
+//FUNCAO PARA ACHAR O CIRCULO DE REFERENCIA
+//MAIS PROXIMO DO MOUSE DO USUARIO
+function achaCirculoMaisProximo (nomeDoTime, distanciaX) {
+
+  var maximo = width;
+  var objeto;
+  d3.selectAll("circle." + nomeDoTime).each(function (d) {
+      var distancia = Math.abs(d3.select(this).attr("cx") - distanciaX);
+      if (distancia < maximo) {
+          maximo = distancia;
+          objeto = d;
+      }
+  })
+
+  return objeto;
+
+}
+
+//LOADING
+$(window).load(function() {
+   $('#loading').hide();
+});
