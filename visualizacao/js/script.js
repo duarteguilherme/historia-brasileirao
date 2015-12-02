4db0f2447f65b6c61ab766e9b8b7549c2ee2c545
Rodrigo Burgarelli <rodrigo.burgarelli1@gmail.com>
2015-12-02 01:06:59 -0200

    comecando a consertar o brush

--- visualizacao/js/script.js
+++ visualizacao/js/script.js
@@ -1,20 +1,10 @@
 //DIMENSOES DO GRAFICO
-var margin = {top: 10, right: 55, bottom: 30, left: 50},
-    // width = 1040 - margin.left - margin.right,
-    w = window.innerWidth - 20,
+var margin = {top: 10, right: 10, bottom: 100, left: 40},
     width = window.innerWidth - margin.right - margin.left - 20,
-    height = 500 - margin.top - margin.bottom;
-    //para o gráfico menor
-    height_2 = 100 - margin.top - margin.bottom;
-
-var largura_barrinha = 5;
-
-//CODIGO REFERENTE AO DRAG
-/*var drag = d3.behavior.drag()
-    .origin(function(d) { return d; })
-    .on("dragstart", dragstarted)
-    .on("drag", dragged)
-    .on("dragend", dragended);*/
+    margin2 = {top: 430, right: 10, bottom: 20, left: 40},
+    height = 500 - margin.top - margin.bottom,
+    height2 = 500 - margin2.top - margin2.bottom,
+    w = window.innerWidth - 20;
 
 //FORMATO DE PARSEMENTO DOS DADOS DE DATA
 var parseData = d3.time.format("%y%m%d").parse;
@@ -23,10 +13,7 @@ var timeEscolhido; //ARMAZENA O TIME ESCOLHIDO PELO USUARIO NO MENU
 var linhaSelecionada; //ARMAZENA A LINHA ESCOLHIDA A PARTIR DO TIME ESCOLHIDO PELO USUARIO NO MENU
 var nomesDosTimes; //ARMAZENA O NOME DOS TIMES; VARIAVEL DE APOIO PARA CHECAGEM E ITERACAO
 var times; //ARMAZENA CADA OBJETO-TIME INTEIRO COM SEUS RESPECTIVOS VALORES (DATAS E INDICES)
-var dados;
-var zoom_atual = [0,0];
-var alca_esquerda, alca_direita;
-var circulos; //ADICIONADOS AO GRAFICO QUANDO O USUARIO SELECIONA UMA LINHA ESPECIFICA
+
 //TOOLTIP COM INFORMACOES A SEREM APRESENTADAS AO USUARIO
 var tooltip = d3.select("body").append("div")
       .attr("class", "infoTooltip")
@@ -37,8 +24,6 @@ var tooltip = d3.select("body").append("div")
 
 //times que têm os escudos à mostra
 var escudos = ['atleticomg','atleticopr','avai','chapecoense','corinthians','coritiba','cruzeiro','figueirense','flamengo','fluminense','goias','gremio','internacional','joinville','palmeiras','pontepreta','santos','saopaulo','sport','vasco']
-
-var traduz_zoom = d3.scale.linear().domain([0,width]).range([20,1]);
 
 //baixa os dados e chama a função de começar
 d3.json ("data/dadosfull.json", comeca_tudo);
@@ -66,7 +51,7 @@ function conserta_dados(data) {
 
     //INSERINDO O NOME DOS CLUBES NO DROPDOWN MENU
     nomesDosTimes.forEach(function(d) {
-        $("ul.sub-menu").append("<li class=\"timeBrasileirao" + " " + d + "\"><a href=\"javascript:void(0)\">" + d + "</a></li>");
+        $("ul.sub-menu").append("<li class='timeBrasileirao'><a href=\"javascript:void(0)\">" + d + "</a></li>");
     });
 
     //CRIANDO UM ARRAY DE OBJETOS COM OS TIMES E SEUS RESPECTIVOS VALORES
@@ -106,24 +91,11 @@ function conserta_dados(data) {
     return times_filtrado;
 }
 
-//funções e variáveis para eixos e linhas
-var make_x_axis = function () {
-    return d3.svg.axis()
-        .scale(x)
-        .orient("bottom")
-        .ticks(5);
-};
-
-var make_y_axis = function () {
-    return d3.svg.axis()
-        .scale(y)
-        .orient("left")
-        .ticks(1);
-};
-
-
 function comeca_tudo(data) {
     times = conserta_dados(data);
+
+    //SELECIONA E MOSTRA A LINHA ESCUDO AO CARREGAR A PÁGINA
+    selecionaLinha($("#menuEscudos01").children()[0].id);
 
     //gambiarra para não dar zoom além dos anos dos dados
     dominio_x = d3.extent(data[1]['data_fake'], function(d) { return d; })
@@ -140,14 +112,27 @@ function comeca_tudo(data) {
         ])
         .range([height, 0]);
 
-    xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom")
-        .ticks(d3.time.years, 1);
+    x2 = d3.time.scale()
+        .domain(x.domain())
+        .range([0, width]);
 
-    yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
+    y2 = d3.scale.linear()
+        .range([height2,0])
+        .domain(y.domain());
+
+    var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+        xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
+        yAxis = d3.svg.axis().scale(y).orient("left");
+
+    var brush = d3.svg.brush()
+        .x(x2)
+        .extent(x2.domain())
+        .on("brush", brushed);
+
+    var arc = d3.svg.arc()
+        .outerRadius(height2 / 2)
+        .startAngle(0)
+        .endAngle(function(d, i) { return i ? -Math.PI : Math.PI; });
 
     line = d3.svg.line()
         .interpolate("cardinal")
@@ -159,348 +144,109 @@ function comeca_tudo(data) {
             return y(d.indice);
         });
 
-    zoom = d3.behavior.zoom()
-        .x(x)
-        .scaleExtent([1, 20])
-        .on("zoom", zoomed);
+    line2 = d3.svg.line()
+        .interpolate("linear")
+        .defined(function(d) { return d.indice != null; })
+        .x(function(d) {
+            return x2(d.data);
+        })
+        .y(function(d) {
+            return y2(d.indice);
+        });
 
-    //primeiro grafico
-    svg = d3.select("#grafico1")
+    svg = d3.select("#grafico")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-        .on("mousedown", clickGrafico)
-        .call(zoom);
 
-    svg.append("rect")
+    svg.append("defs").append("clipPath")
+        .attr("id", "clip")
+        .append("rect")
+        .attr("class","overlay")
         .attr("width", width)
         .attr("height", height)
-        .style("fill", "none")
-        .attr('class','plot')
-        .style("pointer-events", "all");
+        .on("mousemove", mousemove);
 
-    svg.append("svg:g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(xAxis);
+    var focus = svg.append("g")
+        .attr("class", "focus")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
+    context = svg.append("g")
+        .attr("class", "context")
+        .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-    //um tick pra ser a linha da média
-    svg.append("g")
-        .attr("class", "y grid")
-        .call(make_y_axis()
-            .tickSize(-width, 0, 0)
-            .tickFormat(""));
-
-    var clip = svg.append("svg:clipPath")
-        .attr("id", "clip")
-        .append("svg:rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", width)
-        .attr("height", height);
-
-    chartBody = svg.append("g")
-        .attr("clip-path", "url(#clip)")
+    focus.append("g")
         .selectAll('.times')
         .data(times)
         .enter().append("g")
-        .attr("class","times");
-
-    chartBody.append("path")
+        .attr("class", "times")
+        .append('path')
         .attr("class", function (d,i) {
-            return "line " + nomesDosTimes[i]
+            return "line " + d.nome
         })
         .attr("opacity", 0.7)
         .attr("d", function(d) { return line(d.valores); })
-        .style("stroke", function() { return "rgb(127, 127, 127)"; })
-        //tooltip on
         .on("mouseover", mostra_tooltip)
         .on("mouseout", function(d, i) {
             tooltip.style("opacity", 0)
         })
         .on("click", function(d) {
-          console.log("uma linha selecionada com o click do mouse.");
-          selecionaLinha(d.nome);
-          mostraLinha(timeEscolhido, linhaSelecionada, false);
+            console.log("uma linha selecionada com o click do mouse.");
+            selecionaLinha(d.nome);
+            mostraLinha(timeEscolhido, linhaSelecionada, false);
         });
 
-    //ELEMENTO PARA AGRUPAR OS CIRCULOS DE REFERENCIA
-    //circulos = svg.append("g").attr("class", "circulos");
-
-    //agora criamos o grafico menor
-    x_2 = d3.time.scale()
-        .domain(d3.extent(data[1]['data_fake'], function(d) { return d; }))
-        .range([0, width]);
-
-    y_2 = d3.scale.linear()
-        .range([height_2,0])
-        .domain([
-            d3.min(times, function(t) { return d3.min(t.valores, function(v) { return v.indice; }); }),
-            d3.max(times, function(t) { return d3.max(t.valores, function(v) { return v.indice; }); })
-        ]);
-
-    yAxis_2 = d3.svg.axis()
-        .scale(y_2)
-        .orient("left");
-
-    line_2 = d3.svg.line()
-        .interpolate("linear")
-        .defined(function(d) { return d.indice != null; })
-        .x(function(d) {
-            return x_2(d.data);
-        })
-        .y(function(d) {
-            return y_2(d.indice);
-        });
-
-    svg_2 = d3.select("#grafico2")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height_2 + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var dragregua = d3.behavior.drag()
-        .on("drag", move_regua);
-
-    regua = svg_2.append("rect")
-        .attr("width", width)
-        .attr("height", height_2)
-        .style("opacity","0.3")
-        .attr('class','regua')
-        .style("pointer-events", "all")
-        .call(dragregua);
-
-    var dragesquerda = d3.behavior.drag()
-        .on("drag", dragleft);
-
-    alca_esquerda = svg_2.append("rect")
-        .attr("class","alca esquerda")
-        .attr("height", height_2)
-        .attr("width", largura_barrinha)
-        .attr("x",0)
-        .call(dragesquerda);
-
-    var dragdireita = d3.behavior.drag()
-        .on("drag", dragright);
-
-    alca_direita = svg_2.append("rect")
-        .attr("class","alca direita")
-        .attr("height", height_2)
-        .attr("width", largura_barrinha)
-        .attr("x",0)
-        .attr("transform","translate("+(width-largura_barrinha)+",0)")
-        .call(dragdireita);
-
-    svg_2.append("svg:g")
+    focus.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0, " + height_2 + ")")
+        .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
-    //SELECIONA E MOSTRA A LINHA ESCUDO AO CARREGAR A PÁGINA
-    selecionaLinha($("#menuEscudos01").children()[0].id);
-    mostraLinha(timeEscolhido, linhaSelecionada, false);
+    focus.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
 
-    svg_2.append("path")
+    context.append("path")
         .datum(times[timeEscolhido].valores)
         .attr("class", "line_aux")
-        .attr("d", line_2);
+        .attr("d", line2);
+
+    context.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height2 + ")")
+        .call(xAxis2);
+
+    brushg = context.append("g")
+        .attr("class", "x brush")
+        .call(brush);
+
+    brushg.selectAll(".resize").append("path")
+        .attr("transform", "translate(0," +  height2 / 2 + ")")
+        .attr("d", arc);
+
+    brushg.selectAll("rect")
+        .attr("height", height2 );
+
+
+    mostraLinha(timeEscolhido, linhaSelecionada, false);
+
+    function brushed() {
+        x.domain(brush.empty() ? x2.domain() : brush.extent());
+        focus.selectAll(".line").attr("d", function(d) { return line(d.valores); });
+        focus.select(".x.axis").call(xAxis);
+    }
 
     //esconde o loading
     $('#loading').hide();
-}
 
 
-function redesenha() {
-    //muda eixos e grids
-    svg.select(".x.axis").call(xAxis);
-    svg.select(".x.grid").call(xAxis)
-        .call(make_x_axis()
-            .tickSize(-height, 0, 0)
-            .tickFormat(""));
-    svg.select(".y.grid")
-        .call(make_y_axis()
-            .tickSize(-width, 0, 0)
-            .tickFormat(""));
-
-    //muda linhas
-    svg.selectAll(".times").selectAll(".line")
-        .attr("d", function(d) { return line(d.valores); });
-
-    arruma_destaque_linhas();
-    //zoom nos circulos
-    /*circulos.selectAll('.circulo')
-        .attr("cx", function (d) {
-            return x(d.data);
-        })
-        .attr("cy", function (d) {
-            return y(d.indice);
-        })*/
 }
 
 function mostra_tooltip (element, i) {
-    //console.log($('.'+element['nome']));
-    if(zoom.scale() > 4 && $(".circulo").length > 0) {  }
-    else {
-        tooltip.transition()
-            .style("opacity", .9)
-            .text(element['nome'])
-        tooltip
-            .style("left", (d3.event.pageX+5) + "px")
-            .style("top", (d3.event.pageY-30) + "px");
-    }
-}
-
-function zoomed () {
-    zoom.scale(parseInt(zoom.scale()));
-
-    //gambiarra para não dar zoom além dos anos dos dados
-    zoom.translate(panLimit());
-    //checa se houve mudança de posição antes de redesenhar
-    if (zoom.translate()[0] != zoom_atual[0] || zoom.translate()[1] != zoom_atual[1]) {
-        //atualiza o grafico
-        redesenha();
-
-        //muda retangulo de baixo
-        dragleft(x_2(x.domain()[0]));
-        dragright(x_2(x.domain()[1]));
-
-        zoom_atual = zoom.translate();
-    }
-}
-
-//limite no eixo X para o gráfico (parte da gambiarra)
-function panLimit() {
-    var divisor = {h: height / ((y.domain()[1]-y.domain()[0])*zoom.scale()), w: width / ((x.domain()[1]-x.domain()[0])*zoom.scale())},
-        minX = -(((x.domain()[0]-x.domain()[1])*zoom.scale())+(panExtent.x[1]-(panExtent.x[1]-(width/divisor.w)))),
-        minY = -(((y.domain()[0]-y.domain()[1])*zoom.scale())+(panExtent.y[1]-(panExtent.y[1]-(height*(zoom.scale())/divisor.h))))*divisor.h,
-        maxX = -(((x.domain()[0]-x.domain()[1]))+(panExtent.x[1]-panExtent.x[0]))*divisor.w*zoom.scale(),
-        maxY = (((y.domain()[0]-y.domain()[1])*zoom.scale())+(panExtent.y[1]-panExtent.y[0]))*divisor.h*zoom.scale(),
-
-        tx = x.domain()[0] < panExtent.x[0] ?
-            minX :
-                x.domain()[1] > panExtent.x[1] ?
-            maxX :
-            zoom.translate()[0],
-        ty = y.domain()[0]  < panExtent.y[0]?
-            minY :
-                y.domain()[1] > panExtent.y[1] ?
-            maxY :
-            zoom.translate()[1];
-    return [tx,ty];
-}
-
-//função para mover a regua de baixo
-function move_regua(d) {
-
-    var variacao =  d3.event.dx;
-
-    var pos_left = d3.transform(alca_esquerda.attr("transform")).translate[0];
-    var pos_right = d3.transform(alca_direita.attr("transform")).translate[0];
-    var mudar = true;
-
-    //veja se não passou do limite (se passou, não faz nada)
-    if (pos_left == 0) {
-        if (variacao < 0) {
-            mudar = false;
-        }
-    }
-
-    if (pos_right == (width-largura_barrinha)) {
-        if (variacao > 0) {
-            mudar = false;
-        }
-    }
-
-
-    if (mudar) {
-        dragleft(pos_left+variacao,false);
-        dragright(pos_right+variacao,true);
-    }
-}
-
-
-//função[s] para o drag das alças
-function dragleft(d,mudar_grafico) {
-    if (mudar_grafico == 0) {
-        mudar_grafico = true;
-    }
-    var posicao = d3.event.x;
-    var mudar_graficao = true;
-    if (d) {
-        posicao = d;
-        if (!(mudar_graficao)) {
-            mudar_graficao = false;
-        }
-    }
-    var x_orig = d3.transform(alca_esquerda.attr("transform")).translate[0];
-
-    //x novo não pode ser menor que zero
-    var new_x = posicao > 0 ? posicao : 0;
-
-    //tbm não pode ser maior que a posição da alça da direita
-    var x_direita = d3.transform(alca_direita.attr("transform")).translate[0];
-    new_x = new_x < (x_direita - largura_barrinha) ? new_x : (x_direita - largura_barrinha);
-
-    //move barrinha
-    alca_esquerda.attr("transform", "translate(" + new_x + ",0)");
-
-    //move e resize retangulo
-    var delta_x = x_orig - new_x;
-    var width_agora = parseInt(regua.attr("width"));
-
-    regua.attr("width",function () { return width_agora + delta_x });
-    regua.attr("transform", "translate(" + new_x + ",0)");
-
-    //muda a escala do grafico maior
-    if (mudar_graficao) {
-        var novo_dominio = x_2.invert(new_x);
-        x.domain([novo_dominio, x.domain()[1]]);
-        if (mudar_grafico) {
-            redesenha();
-        }
-    }
-}
-
-function dragright(d,mudar_grafico) {
-    if (mudar_grafico == 0) {
-        mudar_grafico = true;
-    }
-
-    var posicao = d3.event.x;
-    var mudar_graficao = true;
-    if (d) {
-        posicao = d;
-        if (!(mudar_graficao)) {
-            mudar_graficao = false;
-        }
-    }
-
-    //x não pode ser maior que o tamanho do grafico
-    var new_x = posicao > (width) ? (width) : posicao;
-
-    //tbm não pode ser menor que a posição da alça da esquerda
-    var x_esquerda = d3.transform(alca_esquerda.attr("transform")).translate[0];
-    new_x = new_x > (x_esquerda + largura_barrinha) ? new_x : (x_esquerda + largura_barrinha);
-
-    //move barrinha
-    alca_direita.attr("transform", "translate(" + new_x + ",0)");
-
-    //move e resize retangulo
-    regua.attr("width",function () { return new_x + largura_barrinha - d3.transform(alca_esquerda.attr("transform")).translate[0] });
-
-    //muda a escala do grafico maior só se tiver sido chamado mexendo no retangulo de baixo, e nao dando zoom em cima
-    if (mudar_graficao) {
-        var novo_dominio = x_2.invert(new_x);
-        x.domain([x.domain()[0],novo_dominio]);
-        if (mudar_grafico) {
-            redesenha();
-        }
-    }
+    tooltip.transition()
+        .style("opacity", .9)
+        .text(element['nome'])
+    tooltip
+        .style("left", (d3.event.pageX+5) + "px")
+        .style("top", (d3.event.pageY-30) + "px");
 }
 
 //FUNCAO PARA ADICIONAR OS ESCUDOS DOS TIMES
@@ -599,10 +345,10 @@ $(document).ready(function(){
 
 function redesenha_linha() {
     d3.selectAll(".line_aux").remove();
-    svg_2.append("path")
+    context.append("path")
         .datum(times[timeEscolhido].valores)
         .attr("class", "line_aux")
-        .attr("d", line_2);
+        .attr("d", line2);
 }
 
 function selecionaLinha (nome) {
@@ -651,9 +397,10 @@ function arruma_destaque_linhas() {
     //AUMENTA A OPACIDADE DA LINHA SELECIONADA
     //ALTERA A COR DA LINHA SELECIONADA
     //DESLOCA ELA PARA CIMA DAS OUTRAS
-    $(linhaSelecionada).attr("opacity", 1);
-    $(linhaSelecionada).css("stroke", "rgb(255, 102, 0)");
-    $(linhaSelecionada).css("stroke-width", function(d) { return "1.5px" });
+    var linha_destaque = $('.'+times[timeEscolhido].nome)
+    linha_destaque.attr("opacity", 1);
+    linha_destaque.css("stroke", "rgb(255, 102, 0)");
+    linha_destaque.css("stroke-width", function(d) { return "1.5px" });
 
 }
 
@@ -733,39 +480,15 @@ function removeCirculos () {
 }
 
 function mousemove() {
+    console.log('cole')
 
-  if(zoom.scale() > 4 && $(".circulo").length > 0) {
+  tooltip.transition()
+  .style("opacity", .9)
+  .text(nomesDosTimes[timeEscolhido] + " : " + circuloMaisProximo.indice)
+  tooltip
+  .style("left", (d3.event.pageX+5) + "px")
+  .style("top", (d3.event.pageY-30) + "px")
 
-    var distanciaMouseX = d3.mouse(this)[0];
-    var circuloMaisProximo = achaCirculoMaisProximo(nomesDosTimes[timeEscolhido], distanciaMouseX);
-
-    if (circuloMaisProximo.indice != null) {
-
-      /* PAREI AQUI
-      var nomeDaLinhaParaMostrar = element.nome;
-      var indexDaLinhaParaMostrar;
-      
-      nomesDosTimes.forEach(function (element, index) {
-        if (nomeDaLinhaParaMostrar == element) {
-          indexDaLinhaParaMostrar = index;
-        }
-      });
-
-      console.log(linhaTime[0][indexDaLinhaParaMostrar].firstChild);
-
-      //d.style("stroke", function(d) { return "rgb(127, 1, 234)"; });
-      */
-
-      tooltip.transition()
-      .style("opacity", .9)
-      .text(nomesDosTimes[timeEscolhido] + " : " + circuloMaisProximo.indice)
-      tooltip
-      .style("left", (d3.event.pageX+5) + "px")
-      .style("top", (d3.event.pageY-30) + "px")
-
-    }
-
-  }
 
 }
 
